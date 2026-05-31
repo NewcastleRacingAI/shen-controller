6f283ea47d1a44ba2dc5c217ffc985284d1eafc6
Gabriel Perry <93535957+GRP-Projects@users.noreply.github.com>
2026-05-31 02:09:01 +0100

    Configured controller for NIMS compatibility

--- nrai_controller/purepursuit.py
+++ nrai_controller/purepursuit.py
@@ -3,14 +3,15 @@ from geometry_msgs.msg import PoseStamped
 
 def get_angle(path: object, drive: object) -> object:
 
-    lookahead_distance = 5
-    max_angle = 21
-    wheel_base = 5
+    lookahead_distance = 2
+    wheel_base = 1.53
 
     path.insert(0, PoseStamped())
     for i in range(0, len(path)-1):
-        gradient = (path[i].pose.position.y - path[i+1].pose.position.y)/(path[i].pose.position.x - path[i+1].pose.position.x)
-        constant = path[i].pose.position.y - (gradient*path[i].pose.position.x)
+        gradient = float(999)
+        if (path[i].pose.position.x - path[i+1].pose.position.x) != 0:
+            gradient = (path[i].pose.position.z - path[i+1].pose.position.z)/(path[i].pose.position.x - path[i+1].pose.position.x)
+        constant = path[i].pose.position.z - (gradient*path[i].pose.position.x)
 
         a = 1 + math.pow(gradient, 2)
         b = 2 * gradient * constant
@@ -22,17 +23,8 @@ def get_angle(path: object, drive: object) -> object:
             xplus = ((-1 * b) + math.sqrt(discriminant)) / (2 * a)
             xminus = ((-1 * b) - math.sqrt(discriminant)) / (2 * a)
 
-            #print("Node " + str(i) + " and " + str(i+1))
-            #print("[" + str(path[i].pose.position.x) + ", " + str(path[i+1].pose.position.x) + "]")
-
-            #print("y = " + str(gradient) + "x + " + str(constant) + " {" + str(min(path[i+1].pose.position.x, path[i].pose.position.x)) + " < x < " + str(max(path[i+1].pose.position.x, path[i].pose.position.x)) + "}")
-
             yplus = (gradient * xplus) + constant
             yminus = (gradient * xminus) + constant
-
-            #print("xplus: " + str(xplus) + " yplus: " + str(yplus))
-            #print("xminus: " + str(xminus) + " yminus: " + str(yminus))
-            #print("Magnitude: " + str(math.sqrt(math.pow(xplus, 2) + math.pow(yplus, 2))))
 
             if min(path[i+1].pose.position.x, path[i].pose.position.x) <= xplus <= max(path[i+1].pose.position.x, path[i].pose.position.x):
                 a = (math.atan(yplus/xplus) % math.pi) - math.pi/2
@@ -42,11 +34,4 @@ def get_angle(path: object, drive: object) -> object:
                 a = (math.atan(yminus/xminus) % math.pi) - math.pi/2
                 angle = math.atan(2 * wheel_base * math.sin(a)/(lookahead_distance))
                 drive.steering_angle = angle
-
-        #print("M: " + str(m))
-        #print("C: " + str(c))
-        #print(path[i].pose.position.x)
-        #print(path[i].pose.position.y)
-        #print(path[i].pose.position.z)
-        #print("~~~~~~~~~~")
     return drive
--- nrai_controller/ros_node.py
+++ nrai_controller/ros_node.py
@@ -20,33 +20,70 @@ from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion
 from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
 from nav_msgs.msg import Path
 
+import struct
+import os
+
 import math
 
 from .purepursuit import get_angle
 
 class MinimalPublisher(Node):
 
-    def __init__(self):
+    def __init__(self, nims: bool):
+        self.nims = nims
         super().__init__('nrai_controller')
         self.publisher_cmd = self.create_publisher(AckermannDriveStamped, '/cmd', 10)
         self.subscriber_path = self.create_subscription(Path, '/path', self.process, 10)
 
     def process(self, msg):
         drive = AckermannDriveStamped()
-        get_angle(msg.poses, drive.drive)
-        #self.publisher_cmd.publish(get_angle(msg.poses))
+        drive.drive = get_angle(msg.poses, drive.drive)
+        drive.drive.steering_angle_velocity = float(1)
+        drive.drive.speed = float(0.5)
+        drive.drive.acceleration = float(0.25)
+        drive.drive.jerk = float(0.1)
+        if self.nims:
+            drive = drive.drive
+            #new_instruction = bytes()
+            #new_instruction += np.float32(drive.steering_angle).tobytes()
+            #new_instruction += np.float32(drive.steering_angle_velocity).tobytes()
+            #new_instruction += np.float32(drive.speed).tobytes()
+            #new_instruction += np.float32(drive.acceleration).tobytes()
+            #new_instruction += np.float32(drive.jerk).tobytes()
+            #new_instruction += bytes(b'\xff\xff\xff\xff')
+            new_instruction = struct.pack('<5fI', drive.steering_angle, drive.steering_angle_velocity, drive.speed, drive.acceleration, drive.jerk, 0xFFFFFFFF)
+            try:
+                fd = os.open("/tmp/lower_ctrl_cmd", os.O_WRONLY)
+                with open(fd, "wb") as file:
+                    file.write(new_instruction)
+            except FileNotFoundError:
+                self.get_logger().info("Could not access FIFO. Likely not yet configured.")
+            except BrokenPipeError:
+                self.get_logger().info("FIFO terminated")
+        else:
+            self.publisher_cmd.publish(drive)
+        
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    minimal_publisher = MinimalPublisher()
+    minimal_publisher = MinimalPublisher(False)
 
     rclpy.spin(minimal_publisher)
 
     minimal_publisher.destroy_node()
     rclpy.shutdown()
 
+def nims(args=None):
+    rclpy.init(args=args)
+
+    minimal_publisher = MinimalPublisher(True)
+
+    rclpy.spin(minimal_publisher)
+
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
